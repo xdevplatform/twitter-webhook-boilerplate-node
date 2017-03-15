32f8e1d1226501f9b4b8f86d28d17c2f72dfd4b3
jcipriano <jcipriano@twitter.com>
2017-03-15 15:44:53 -0700

    Fixing hmac generation.

--- security.js
+++ security.js
@@ -12,9 +12,7 @@ security.get_challenge_response = function(crc_token, consumer_secret) {
 
   hmac = crypto.createHmac('sha256', consumer_secret).update(crc_token).digest('base64');
 
-  response_token = "sha256=" + hmac
-
-  return response_token;
+  return hmac;
 }
 
 
