67a24bfb3bc72fe132a2a22bf17e12f00a2d0cca
jcipriano <jcipriano@twitter.com>
2017-06-01 13:47:38 -0700

    Cleans up npms and semicolon use

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
-var express = require('express');
+var express = require('express')
 var security = require('./security')
-var bodyParser = require('body-parser');
-var node_url = require('url');
-var nconf = require('nconf');
-var Twit = require('twit');
-var app = express();
+var bodyParser = require('body-parser')
+var nconf = require('nconf')
+var app = express()
 
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
 
 
 // load config
 nconf.file({ file: 'config.json' }).env();
 
-// setup Twitter API client
 var twitter_config = {
   consumer_key: nconf.get('TWITTER_CONSUMER_KEY'),
   consumer_secret: nconf.get('TWITTER_CONSUMER_SECRET'),
   access_token: nconf.get('TWITTER_ACCESS_TOKEN'),
   access_token_secret: nconf.get('TWITTER_ACCESS_TOKEN_SECRET')
-};
-var twitter = new Twit(twitter_config);
+}
 
 
 /**
  * Serves the home page
  **/
 app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+  response.render('pages/index')
+})
 
 
 /**
@@ -43,14 +39,14 @@ app.get('/', function(request, response) {
  **/
 app.get('/webhooks/twitter', function(request, response) {
 
-  var crc_token = request.query.crc_token;
+  var crc_token = request.query.crc_token
 
-  var hash = security.get_challenge_response(crc_token, twitter_config.consumer_secret);
+  var hash = security.get_challenge_response(crc_token, twitter_config.consumer_secret)
 
   response.send({
     response_token: 'sha256=' + hash
-  });
-});
+  })
+})
 
 
 /**
@@ -58,15 +54,17 @@ app.get('/webhooks/twitter', function(request, response) {
  **/
 app.post('/webhooks/twitter', function(request, response) {
 
+  // Your custom bot logic will start here
+
   console.log(request.body)
 
-  response.send('200 OK');
+  response.send('200 OK')
   
-});
+})
 
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+  console.log('Node app is running on port', app.get('port'))
+})
 
 
--- security.js
+++ security.js
@@ -1,6 +1,6 @@
-crypto = require('crypto');
+crypto = require('crypto')
 
-var security = {};
+var security = {}
 
 /**
  * Creates a HMAC SHA-256 hash created from the app TOKEN and
@@ -10,10 +10,10 @@ var security = {};
  */
 security.get_challenge_response = function(crc_token, consumer_secret) {
 
-  hmac = crypto.createHmac('sha256', consumer_secret).update(crc_token).digest('base64');
+  hmac = crypto.createHmac('sha256', consumer_secret).update(crc_token).digest('base64')
 
-  return hmac;
+  return hmac
 }
 
 
-module.exports = security;
+module.exports = security
